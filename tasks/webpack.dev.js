726e7def095c20538fa19afc2141da7ed05c8a99
David Wincent <david.wincent@decerno.se>
2021-04-09 06:02:17 +0000

    webpack config

--- tasks/webpack.dev.js
+++ tasks/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = merge(common, {
   output: {
@@ -12,8 +11,8 @@ module.exports = merge(common, {
     library: 'Castor'
   },
   mode: 'development',
+  devtool: 'source-map',
   module: {},
-  plugins: [new WriteFilePlugin()],
   devServer: {
     contentBase: './',
     port: 9009
